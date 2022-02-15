dc9a0979ca5a30dc12dcd527b27b11b4df49f9e4
GilbyKoloay <lolwtf10001@gmail.com>
2022-02-15 20:19:29 +0800

    Added http, moment, server.

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const http = require('http');
+const moment = require('moment');
+
+const server = http.createServer((req, res) => {
+    res.statusCode= 200;
+    res.setHeader('Content-type', 'text/html');
+
+
+
+    res.end();
+});
+
 const hostname = 'localhost';
 const port = 3000;
 server.listen(port, hostname, () => {
     console.log(`Server running at https://${hostname}:${port}/`);
 });
+
