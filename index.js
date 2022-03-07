0f8838f997b39a98e3baf5a63c04cfb20a1b8f1f
Enriquesheriman <s11910257@student.unklab.ac.id>
2022-03-07 18:12:11 +0800

    menambahkan delete dan error handling

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// DELETE users name
+app.delete('/users/:name', (req, res) => {
+    if(users.filter(r => r.name.toLowerCase() === req.params.name.toLowerCase()).length === 0) {
+        res.send(JSON.stringify({
+            message: "Data user tidak ditemukan."
+        }));
+    }
+    else {
+        users = users.filter(r => r.name.toLowerCase() !== req.params.name.toLowerCase());
+        res.send(users);
+    }
+});
 
+// Error handling
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send(JSON.stringify({
+        status: 'error',
+        message: "terjadi kesalahan pada server."
+    }));
+});
 
 app.listen(port, () => console.log(`Server is running at http://localhost:${port}.`));
+
