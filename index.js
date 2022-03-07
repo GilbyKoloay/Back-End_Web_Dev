1df8007ab3280fd1beefc569a42615f96f9c45ca
KoloayGilby <lolwtf10001@gmail.com>
2022-03-07 18:44:43 +0800

    added GET Default

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// GET Default
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Group 3</h1>
+        <p>Steffano Immanuel Rondonuwu</p>
+        <p>Anastasya Alicia Sumayku</p>
+        <p>Enrique Giovanny Imanuel Sheriman</p>
+        <p>Gilby Ezra Albert Koloay</p>
+        <p>Stevali Mercella Item</p>
+    `);
+});
+
 // DELETE users name
 app.delete('/users/:name', (req, res) => {
     if(users.filter(r => r.name.toLowerCase() === req.params.name.toLowerCase()).length === 0) {
